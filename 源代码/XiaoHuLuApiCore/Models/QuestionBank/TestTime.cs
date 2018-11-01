a3cfaa92ee773853ddbfa566865a985028c77f4b
WeiZiboSheep <wzblyl@163.com>
2018-11-01 21:22:05 +0800

    xioahulu

---
+++ 源代码/XiaoHuLuApiCore/Models/QuestionBank/TestTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.QuestionBank
+{
+    public class TestTime
+    {
+        /// <summary>
+        /// 考试时间主键
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 准考证号码
+        /// </summary>
+        public int ExamNumberId { get; set; }
+
+        /// <summary>
+        /// 考试总时长
+        /// </summary>
+        public int LongExam { get; set; }
+
+        /// <summary>
+        /// 所用时长
+        /// </summary>
+        public int TimeUsed { get; set; }
+
+        /// <summary>
+        /// 剩余时长
+        /// </summary>
+        public int RemainderLength { get; set; }
+
+        /// <summary>
+        /// 答题进度
+        /// </summary>
+        public int ProgressOfAnswer { get; set; }
+    }
+}
