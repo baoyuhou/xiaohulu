44ea6eb54d88c1a4e968f7910220289d5479f2fe
Sunyq2893 <17310032326@163.com>
2018-11-02 20:29:51 +0800

    孙雨琪

---
+++ 源代码/XiaoHuLuApiCore/Models/Authority/UsersInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Authority
+{
+    /// <summary>
+    /// 用户从表
+    /// </summary>
+    public class UsersInfo
+    {
+        /// <summary>
+        /// 角色名
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ 源代码/XiaoHuLuApiCore/Services/Authority.Services/UsersServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Models.Authority;
+using Models;
+using IServices.IAuthority;
+using SqlSugar;
+
+namespace Services.Authority.Services
+{
+    public class UsersServices : IUserServices
+    {
+        public int Add(Users user)
+        {
+            using (SqlSugarClient db = Educationcontext.GetClient())
+            {
+                var result = db.Insertable(user).ExecuteCommand();
+                return result;
+            }
+        }
+
+        public List<UsersInfo> GetUsersList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Users user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
