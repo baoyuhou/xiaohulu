0af41d8ccaacf868fabab1c1c0935dd0aeb890a3
baoyuhou <44572138+baoyuhou@users.noreply.github.com>
2018-11-02 10:08:08 +0800

    zjh

--- 源代码/XiaoHuLuApiCore/Models/Educationcontext.cs
+++ 源代码/XiaoHuLuApiCore/Models/Educationcontext.cs
@@ -1,9 +1,28 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
+using System.Linq;
 
 namespace Models
 {
     public class Educationcontext
     {
+        public static SqlSugarClient GetClient()
+        {
+            SqlSugarClient db = new SqlSugarClient(
+                new ConnectionConfig()
+                {
+                    ConnectionString = "Database=education;Data Source=169.254.152.246;Port=3306;User Id=root;Password=123456",
+                    DbType = DbType.MySql,
+                    IsAutoCloseConnection = true
+                }
+                );
+            db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" + db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                Console.WriteLine();
+            };
+            return db;
+        }
 
     }
 }
