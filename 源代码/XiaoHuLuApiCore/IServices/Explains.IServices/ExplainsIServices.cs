312528b85c604f62f33c435f73e8894cabbf78a8
baoyuhou <44572138+baoyuhou@users.noreply.github.com>
2018-11-02 11:25:32 +0800

    zjh

---
+++ 源代码/XiaoHuLuApiCore/IServices/Explains.IServices/ExplainsIServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IServices
+{
+   public interface ExplainsIServices
+    {
+        /// <summary>
+        /// 获取所有说明信息
+        /// </summary>
+        /// <returns></returns>
+        IList<Models.Explain> GetExplainList();
+    }
+}
---
+++ 源代码/XiaoHuLuApiCore/Services/Explains.Services/ExplainsServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+using SqlSugar;
+
+namespace Services.Explains.Services
+{
+    public class ExplainsServices : IServices.ExplainsIServices
+    {
+        /// <summary>
+        /// 获取所有说明信息
+        /// </summary>
+        /// <returns></returns>
+        public IList<Explain> GetExplainList()
+        {
+            using (SqlSugarClient sqlsc=Educationcontext.GetClient())
+            {
+                var explainList = sqlsc.Queryable<Explain>();
+                return explainList as IList<Explain>;
+            }
+        }
+    }
+}
