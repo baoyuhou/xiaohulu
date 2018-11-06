c76603dc975daa66dfaadfbb70acc3860ce25983
Sunyq2893 <17310032326@163.com>
2018-11-06 08:41:43 +0800

    孙雨琪

--- 源代码/XiaoHuLuApiCore/XiaoHuLuApiCore/Controllers/UserController.cs
+++ 源代码/XiaoHuLuApiCore/XiaoHuLuApiCore/Controllers/UserController.cs
@@ -5,11 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using Models;
+using Models.Authority;
+using IServices.IAuthority;
 namespace XiaoHuLuApiCore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserServices _userServicesl;
+        public UserController(IUserServices userServices)
+        {
+            _userServicesl = userServices;
+        }
+
+        /// <summary>
+        /// 获取用户角色信息
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetUserList")]
+        public List<UsersInfo> GetUserList()
+        {
+            var userList = _userServicesl.GetUsersList();
+            return userList;
+        }
     }
 }
