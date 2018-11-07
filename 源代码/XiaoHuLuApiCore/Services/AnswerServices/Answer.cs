a335d126e9e5f7826417628deddcd26693635469
jcywork <jcyjob@163.com>
2018-11-07 20:29:03 +0800

    jcy

---
+++ 源代码/XiaoHuLuApiCore/Services/AnswerServices/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IServices.IAnswerServices;
+using Models;
+using Models.Examination;
+using SqlSugar;
+
+namespace Services.AnswerServices
+{
+    public class Answer : IAnswer
+    {
+        public List<AnswerModel> GetAnswerModelList(int Id)
+        {
+            using (SqlSugarClient db = Educationcontext.GetClient())
+            {
+                var answermodellist = db.Ado.SqlQuery<AnswerModel>("select * from QuestionBank a INNER JOIN TextType b on a.TypeOfExam=b.ID INNER JOIN `Option` c on a.Id=c.QuestionBankId WHERE a.Id="+Id);
+                return answermodellist;
+            }
+        }
+    }
+}
