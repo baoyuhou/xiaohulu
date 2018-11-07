cdcaaf04c9e95acc73f0250b4a6d26c2b3afd9dc
WeiZiboSheep <wzblyl@163.com>
2018-11-07 11:24:19 +0800

    xiaohulu

--- 源代码/XiaoHuLuApiCore/XiaoHuLuApiCore/Controllers/ExaminationController.cs
+++ 源代码/XiaoHuLuApiCore/XiaoHuLuApiCore/Controllers/ExaminationController.cs
@@ -10,7 +10,7 @@ using Models.Examination;
 using IServices.Examination.IServices;
 namespace XiaoHuLuApiCore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ExaminationController : ControllerBase
     {
