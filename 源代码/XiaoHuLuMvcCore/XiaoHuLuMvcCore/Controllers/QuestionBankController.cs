e77229da3775d375ba6523071721f041fbc0e5d1
WeiZiboSheep <wzblyl@163.com>
2018-11-08 15:07:25 +0800

    魏子博

---
+++ 源代码/XiaoHuLuMvcCore/XiaoHuLuMvcCore/Controllers/QuestionBankController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XiaoHuLuMvcCore.Controllers
+{
+    public class QuestionBankController : Controller
+    {
+        /// <summary>
+        /// 题库管理
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult QuestionsManagement()
+        {
+            return View();
+        }
+
+        public IActionResult text()
+        {
+            return View();
+        }
+
+    }
+}
