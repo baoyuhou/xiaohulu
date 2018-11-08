1a0445df0b586b05cb7da98d3b6ca8bb6d8dc3ab
baoyuhoude <shandian123>
2018-11-08 08:26:01 +0800

    zjh

--- 源代码/XiaoHuLuMvcCore/XiaoHuLuMvcCore/Controllers/ExplainController.cs
+++ 源代码/XiaoHuLuMvcCore/XiaoHuLuMvcCore/Controllers/ExplainController.cs
@@ -12,7 +12,7 @@ namespace XiaoHuLuMvcCore.Controllers
     public class ExplainController : Controller
     {
         /// <summary>
-        /// 所有说明信息列表
+        /// 显示控制器方法
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
@@ -20,12 +20,20 @@ namespace XiaoHuLuMvcCore.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 所有说明信息列表
+        /// </summary>
+        /// <returns></returns>
         public string GetList()
         {
             var result = WebApiHelper.GetApiResult("get", "Explain", "GetExplainList");
             return result;
         }
 
+        /// <summary>
+        /// 添加控制器视图
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Create()
         {
             return View();
@@ -43,6 +51,23 @@ namespace XiaoHuLuMvcCore.Controllers
                 }
             }
             return View(explain);
+        }
+
+        /// <summary>
+        /// 详情页
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Details(int id)
+        {
+            var result = WebApiHelper.GetApiResult("get","explain", "GetExplainById/" + id);
+            return View(JsonConvert.DeserializeObject<Explain>(result));
+        }
+
+
+        public void SubDetails()
+        {
+            
         }
     }
 }
