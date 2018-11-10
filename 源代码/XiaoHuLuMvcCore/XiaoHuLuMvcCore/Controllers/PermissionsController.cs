c2267f9ebad22d5b140917a02b1b5a8d790dc197
Sunyq2893 <17310032326@163.com>
2018-11-10 14:55:38 +0800

    syq

--- 源代码/XiaoHuLuMvcCore/XiaoHuLuMvcCore/Controllers/PermissionsController.cs
+++ 源代码/XiaoHuLuMvcCore/XiaoHuLuMvcCore/Controllers/PermissionsController.cs
@@ -7,18 +7,47 @@ using Microsoft.AspNetCore.Mvc;
 using XiaoHuLuMvcCore.Models.Authoritys;
 using Newtonsoft.Json;
 using XiaoHuLuMvcCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace XiaoHuLuMvcCore.Controllers
 {
     public class PermissionsController : Controller
     {
         /// <summary>
-        /// 显示全部信息
+        /// 显示角色和权限信息
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
         {
-            var result = WebApiHelper.GetApiResult("Get", "Permissions", "GetJurisList");
-            return View(JsonConvert.DeserializeObject<List<Jurisdictions>>(result));
+            var result = WebApiHelper.GetApiResult("Get", "Role", "GetRoleList");
+
+            var jurisResult = WebApiHelper.GetApiResult("Get", "Permissions", "GetJurisdictions");
+            ViewBag.jurisResult = JsonConvert.DeserializeObject<List<Jurisdictions>>(jurisResult);
+            return View(JsonConvert.DeserializeObject<List<JurisdictionInfo>>(result));
+        }
+
+        /// <summary>
+        /// 单条添加角色和权限信息
+        /// </summary>
+        /// <param name="jurisdictionInfo"></param>
+        /// <returns></returns>
+        public string RolesAdd(JurisdictionInfo jurisdictionInfo)
+        {
+            var result = WebApiHelper.GetApiResult("Post", "Role", "RoleAdd");
+            return result;
+        }
+
+        public void GetRoleViewBag()
+        {
+            var jurisResult = WebApiHelper.GetApiResult("Get", "Permissions", "GetJurisdictions");
+            List<Jurisdictions> list = JsonConvert.DeserializeObject<List<Jurisdictions>>(jurisResult);
+            var linq = from s in list
+                       select new SelectListItem
+                       {
+                           Text = s.Level.ToString(),
+                           Value = s.Id.ToString()
+                       };
+            ViewBag.roleviewbag = linq.ToList();
         }
     }
 }
