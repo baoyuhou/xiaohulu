798048c76d9af2ac3d997142ab7293e2096a6948
jcywork <jcyjob@163.com>
2018-11-12 13:31:06 +0800

    jcy

--- 源代码/XiaoHuLuApiCore/Services/AnswerServices/Answer.cs
+++ 源代码/XiaoHuLuApiCore/Services/AnswerServices/Answer.cs
@@ -21,8 +21,7 @@ namespace Services.AnswerServices
         {
             using (SqlSugarClient db = Educationcontext.GetClient())
             {
-                //var answermodellist = db.Ado.SqlQuery<AnswerModel>("select * from QuestionBank a INNER JOIN TextType b on a.TypeOfExam=b.ID INNER JOIN `Option` c on a.Id=c.QuestionBankId WHERE a.Id="+Id);
-                //return answermodellist;
+                var answermodels = db.Ado.SqlQuery<AnswerModel>("select * from QuestionBank a INNER JOIN TextType b on a.TypeOfExam=b.ID INNER JOIN `Option` c on a.Id=c.QuestionBankId  ORDER BY  RAND()");
                 //var answermodellist = db.Ado.UseStoredProcedure().GetDataTable("p_paging", new {
                 //    @tableName = pageparams.TableName,
                 //    @indexCol = pageparams.IndexCol,
@@ -33,26 +32,26 @@ namespace Services.AnswerServices
                 //    @columns= pageparams.Columns
                 //});
 
-                var answermodels = db.Queryable<QuestionBank, TextType, Option>((questionbank, texttype, op) => new object[] {
-            JoinType.Inner,questionbank.TypeOfExam==texttype.ID,
-            JoinType.Inner,questionbank.Id==op.QuestionBankId
-            })
-               //    .Where((questionbank, texttype, op) => questionbank.Id == Id)
-               .Select((questionbank, texttype, op) => new AnswerModel
-               {
-                   Id = questionbank.Id,
-                   Subject = questionbank.Subject,
-                   Answer = questionbank.Answer,
-                   Photo = questionbank.Photo,
-                   TypeOfExam = questionbank.TypeOfExam,
-                   Enable = questionbank.Enable,
-                   ExamType = texttype.ExamType,
-                   AnswerA = op.AnswerA,
-                   AnswerB = op.AnswerB,
-                   AnswerC = op.AnswerC,
-                   AnswerD = op.AnswerD,
-                   AnswerE = op.AnswerE
-               }).ToList();
+            //    var answermodels = db.Queryable<QuestionBank, TextType, Option>((questionbank, texttype, op) => new object[] {
+            //JoinType.Inner,questionbank.TypeOfExam==texttype.ID,
+            //JoinType.Inner,questionbank.Id==op.QuestionBankId
+            //})
+            //   //    .Where((questionbank, texttype, op) => questionbank.Id == Id)
+            //   .Select((questionbank, texttype, op) => new AnswerModel
+            //   {
+            //       Id = questionbank.Id,
+            //       Subject = questionbank.Subject,
+            //       Answer = questionbank.Answer,
+            //       Photo = questionbank.Photo,
+            //       TypeOfExam = questionbank.TypeOfExam,
+            //       Enable = questionbank.Enable,
+            //       ExamType = texttype.ExamType,
+            //       AnswerA = op.AnswerA,
+            //       AnswerB = op.AnswerB,
+            //       AnswerC = op.AnswerC,
+            //       AnswerD = op.AnswerD,
+            //       AnswerE = op.AnswerE
+            //   }).ToList();
                 return answermodels;
             }
         }
